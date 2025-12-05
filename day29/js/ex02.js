e14556addfb3ae8b8791064936e0222de83280b5
Hoang An Unicode <hoangan.web@gmail.com>
2025-12-05 14:31:20 +0700

    Update code

--- day29/js/ex02.js
+++ day29/js/ex02.js
@@ -2,33 +2,47 @@ const BASE_URL = "https://dummyjson.com";
 const app = {
   _query: {
     order: "desc",
+    limit: 10,
+    page: 1,
   },
   _timeoutId: null,
   init() {
     this.getUsers();
     this.search();
     this.sort();
+    this.paginate();
   },
   async getUsers() {
     try {
       //Add loading
       this.renderLoading();
-      let url = `${BASE_URL}/posts?sortBy=id&order=${this._query.order}`;
+      const skip = (this._query.page - 1) * this._query.limit;
+      let url = `${BASE_URL}/posts?sortBy=id&order=${this._query.order}&limit=${this._query.limit}&skip=${skip}`;
       if (this._query.q) {
-        url = `${BASE_URL}/posts/search?q=${this._query.q}&sortBy=id&order=${this._query.order}`;
+        url = `${BASE_URL}/posts/search?q=${this._query.q}&sortBy=id&order=${this._query.order}&limit=${this._query.limit}&skip=${skip}`;
       }
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Failed to fetch /posts");
       }
       const data = await response.json();
+      const pageNumber = Math.ceil(data.total / this._query.limit);
       this.renderPosts(data.posts);
+      this.renderPaginate(pageNumber);
     } catch (error) {
       //Add error
       this.renderError(error.message);
     } finally {
       //Remove loading
       this.renderLoading(false);
+    }
+  },
+  renderPaginate(pageNumber) {
+    const paginateEl = document.querySelector(".js-paginate");
+    paginateEl.innerHTML = "";
+    for (let page = 1; page <= pageNumber; page++) {
+      const active = this._query.page === page ? "bg-green-600" : "";
+      paginateEl.innerHTML += `<button class="border border-gray-300 px-4 py-2 ${active}">${page}</button>`;
     }
   },
   renderLoading(status = true) {
@@ -79,6 +93,7 @@ const app = {
       this._timeoutId = setTimeout(() => {
         const keyword = e.target.value;
         this._query.q = keyword;
+        this._query.page = 1;
         this.getUsers();
       }, 500);
     });
@@ -110,6 +125,18 @@ const app = {
       });
     });
   },
+  paginate() {
+    const paginateEl = document.querySelector(".js-paginate");
+    paginateEl.addEventListener("click", (e) => {
+      const page = +e.target.innerText;
+      this._query.page = page;
+      window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
+      this.getUsers();
+    });
+  },
 };
 
 app.init();
@@ -127,3 +154,25 @@ app.init();
 // const dbFn = debounce(func1);
 // dbFn(1,2,3) //a, b, c
 //Closure
+
+//Phân trang
+// - Backend --> giới hạn (Mỗi trang 10 bản ghi)
+// - BackEnd --> nhận params là page hoặc offset, skip
+
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+
+//limit = 3, skip = 0 --> 1,2,3 --> page = 1
+//limit = 3, skip = 3 --> 4,5,6 --> page = 2
+//limit = 3, skip = 6 --> 7 --> page = 3
+
+//Convert page -> skip
+// skip = (page - 1) * limit
+
+//Upload file
+//Authentication, Authorization
