575c177af8f9ae5f7d4a23f7a6f8db263fc2fdad
Hoang An Unicode <hoangan.web@gmail.com>
2026-04-17 14:09:51 +0700

    Update code

---
+++ day53/nextjs01/src/app/_components/AppProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { createContext, useState } from "react";
+type AppContext = {
+  message: string;
+  setMessage: (value: string) => void;
+};
+export const AppContext = createContext({} as AppContext);
+export default function AppProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  console.log("app provider");
+
+  const [message, setMessage] = useState("Hello anh em");
+  return (
+    <AppContext.Provider
+      value={{
+        message,
+        setMessage,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
---
+++ day53/nextjs01/src/app/page.tsx
@@ -0,0 +1,17 @@
+// import Button from "./components/Button";
+
+export default function HomePage() {
+  console.log("HomePage");
+  return (
+    <div>
+      <h1 className="text-3xl">Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit iure
+        aliquam repellendus, facilis ipsa ut. Sequi odit harum ratione nemo
+        quisquam reiciendis dolor laborum ex, voluptates ducimus sit iure
+        aliquam!
+      </p>
+      {/* <Button initCount={10} /> */}
+    </div>
+  );
+}
